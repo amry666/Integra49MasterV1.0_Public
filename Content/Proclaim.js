f566a19d7c0d8b83849aa0a501d5477358db7867
Amry Maftuh <amry.maftuh85@gmail.com>
2020-01-13 14:21:33 +0700

    Awal 2020

---
+++ Content/Proclaim.js
@@ -0,0 +1,157 @@
+﻿
+
+if (localStorage.d == "1") {
+    var btnde = '<button type="button" class="btn_hapus btn btn-danger btn-circle waves-effect"> <span><i class="fa fa-times"></i></span></button>';
+    var btnde_nest = '<button type="button" class="btn_hapus btn btn-danger waves-effect btn_nest_ubah float-right"> <span><i class="fa fa-times"></i></span></button>';
+    var checkd = '<input data-checkbox="icheckbox_flat-red" style="cursor:pointer;" type="checkbox" class="checklist check" value="">';
+    var checkdall = '<input style="cursor:pointer;" type="checkbox" class="checklistAll check" value="">';
+}
+else {
+    var btnde = '';
+    var btnde_nest = '';
+    var checkd = '';
+    var checkdall = '';
+}
+
+if (localStorage.u == "1") {
+    var btnup = '<button type="button" data-toggle="modal" data-target="#ModalAddUpdate" class="btn_edit btn btn-success btn-circle waves-effect"><span><i class="ti-pencil-alt"></i></span></button>';
+    var btnup_nest = '<button type="button" data-toggle="modal" data-target="#ModalAddUpdate" class="btn_edit btn btn-success waves-effect btn_nest_ubah float-right"><span><i class="ti-pencil-alt"></i></span></button>';
+} else {
+    var btnup = '';
+}
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+//FILTER DROPDOWN
+function Select2tree_bysp(spquery, element, placeholder) {
+    $.ajax
+        ({
+            url: "../CRUD/R_Proc",
+            type: "GET",
+            data: spquery + "&t=" + $.cookie('t'),
+            cache: !0,
+            success: function (response) {
+                var data2 = $.parseJSON(response);
+                var html = "";
+                for (var x = 0; x < data2.length; x++) {
+                    html += "<option parent='" + data2[x].PARENTID + "' level='" + data2[x].LEVEL + "' value='" + data2[x].ID + "'>" + data2[x].KODE + " " + data2[x].NAMA + "</option>";
+                }
+                $(element).empty();
+                $(element).append(html);
+            }
+        })
+}
+
+//FILTER DROPDOWN
+function Select2_bytbl(tb,element,placeholder) {
+    $.ajax
+        ({
+            url: "../CRUD/R_Table",
+            contentType: "application/html; charset=utf-8",
+            type: "GET",
+            data: "tb=" + tb + "&t=" + $.cookie('t'),
+            cache: !0,
+            datatype: "html",
+            success: function (r) {
+                var data = $.parseJSON(r);
+                var html = "";
+                for (var i = 0; i < data.length; i++) {
+                    html += "<option value='" + data[i].ID + "' >" + data[i].NAMA + "</option>";
+                }
+                $(element).empty();
+                $(element).append('<option value="">' + placeholder+'</option>');
+                $(element).append(html);
+            }
+        })
+}
+
+function Select2_bysp_story(spquery, element, placeholder) {
+    
+    
+    $.ajax
+        ({
+            url: "../CRUD/R_Proc",
+            type: "GET",
+            data: spquery + "&t=" + $.cookie('t'),
+            success: function (r) {
+                var data = $.parseJSON(r);
+                var html = "";
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].ID == 0) {
+                        var id = "";
+                    }
+                    else {
+                        var id = data[i].ID;
+                    }
+                    var date = moment(data[i].CREATEDDATE).locale('id');
+                    html += "<option validdate='" + data[i].VALIDDATE + "' ket='" + data[i].KET + "' stat_upload='" + data[i].STAT_UPLOAD + "' value='" +id + "' >" + date.format('dddd, Do MMMM YYYY, h:mm:ss a') + " (" + data[i].ID + ")</option>";
+                }
+                $(element).empty();
+                $(element).append('<option value="">' + placeholder + '</option>');
+                $(element).append(html);
+            }
+        })
+}
+
+
+//FILTER DROPDOWN CHAINED
+var chained_el = [];
+function Select2chain_bytbl(tb, element, element2, placeholder) {
+    var IDNAME = $(element2).attr('name');
+    chained_el.push(element);
+
+    $(element2).on('change', function(){
+        var ID = $(this).val();
+        $.ajax
+            ({
+                url: "../CRUD/R_Table",
+                contentType: "application/html; charset=utf-8",
+                type: "GET",
+                data: "tb=" + tb + "&" + IDNAME + "=" + ID + "&t=" + $.cookie('t'),
+                cache: !0,
+                datatype: "html",
+                success: function (r) {
+                    var data = $.parseJSON(r);
+                    var html = "";
+                    for (var i = 0; i < data.length; i++) {
+                        html += "<option value='" + data[i].ID + "' >" + data[i].NAMA + "</option>";
+                    }
+                    $(element).empty();
+                    $(element).append('<option value="">' + placeholder + '</option>');
+                    $(element).append(html);
+                    $(element).addClass('chained');
+                }
+            })
+    })
+   
+}
+
+
+$(document).ready(function () {
+    toastr.options = {
+        "closeButton": true, // true/false
+        "debug": false, // true/false
+        "newestOnTop": false, // true/false
+        "progressBar": true, // true/false
+        "positionClass": "toast-top-right", // toast-top-right / toast-top-left / toast-bottom-right / toast-bottom-left
+        "preventDuplicates": false, //true/false
+        "onclick": null,
+        "showDuration": "300", // in milliseconds
+        "hideDuration": "1000", // in milliseconds
+        "timeOut": "1500", // in milliseconds
+        "extendedTimeOut": "1000", // in milliseconds
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+});
---
+++ Content/Proclaim_crud_datatables_story.js
@@ -0,0 +1,377 @@
+﻿
+$(document).ready(function () {
+
+    $(".select2").select2({ width: '100%' });
+    setTimeout(function () { $('.select2tree').select2tree({ width: '100%' }); }, 20);
+
+    //RESET FORM
+    $(".close,#btnbatal").on('click', function () {
+        $("#form_modal2").trigger("reset");
+    });
+
+
+    ////DT remember page
+    function rememberpage() {
+        $('.paginate_button').on('click', function () {
+            localStorage.lastpage = $(this).text();
+        })
+    }
+
+    ////CRUD checkbox
+    function checkbox() {
+        $('.checklist').iCheck({
+            checkboxClass: 'icheckbox_flat-red'
+        });
+        $('.checklistAll').iCheck({
+            checkboxClass: 'icheckbox_flat-red'
+        });
+
+        $(".checklistAll").on('ifChecked', function () {
+            $("input:checkbox.checklist").iCheck('check');
+        });
+
+        $(".checklistAll").on('ifUnchecked', function () {
+            $("input:checkbox.checklist").iCheck('uncheck');
+        });
+
+        $(".checklist").on('ifChanged', function () {
+            var countcheck = $('#DT_Table').find('tbody').find('input:checkbox:checked').length;
+            $('#countcheck').text(countcheck);
+            if (this.checked) {
+                $("#aksicheck").slideDown("2000", function () {
+                    $("#aksicheck").removeClass('hidden');
+                });
+            }
+            else if ($(":checkbox:checked").length === 0) {
+                $("#aksicheck").fadeOut("700", function () {
+                    $("#aksicheck").addClass('hidden');
+                });
+            }
+        });
+    }
+    
+
+    $.each(CRUDFilter, function (index, value) {
+        localStorage.setItem(value, "");
+    });
+    var read_column = [];
+    
+    $.ajax({
+        type: "GET",
+        url: '../CRUD/R_Proc?SP=SYS_GET_COLUMN&tb=' + getUrlVars()["p"] +'&t=' + $.cookie('t'),
+        dataType: 'json',
+        success: function (response) {
+            for (var i = 0; i < response.length; i++) {
+                read_column.push({
+                    data: response[i].COLUMN_NAME,
+                    name: response[i].COLUMN_NAME,
+                    title: response[i].COLUMN_NAME
+                });
+            }
+            
+            setTimeout(function () {
+                Read(),
+                Create();
+            }, 100);
+        }
+    });
+
+    ////////////////////////////////////////////////////////READ BEGIN////////////////////////////////////////////////////////
+    function Read() {
+        $.each(CRUDFilter, function (index, value) {
+            $('#DT_filter' + value).on('change', function () {
+                localStorage.setItem(value, $(this).val());
+                table.draw();
+            });
+        });
+        ////////DATATABLE USER PARAM
+        ////////DATATABLE INITIALISATION
+        table = $('#DT_Table').DataTable({
+            "responsive": true,
+            "processing": true,
+            "serverSide": true,
+            "filter": true,
+            "bInfo": true,
+            "sPaginationType":
+            "simple_numbers",
+            "paging": true,
+            //"fixedHeader": { headerOffset: 20 },
+            "pageLength": 10,
+            "deferLoading": 0,
+            dom: 'lBfrtip',
+            "buttons": [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5',
+                'print',
+                'colvis'
+            ],
+            "ajax": {
+                "url": CRUDURL,
+                "type": "POST",
+                "data": function (data) {
+                    data.t = $.cookie('t');
+                    data.SP = CRUDSP;
+                    $.each(CRUDFilter, function (index, value) {                        
+                        data[value] = localStorage.getItem(value);
+                    });
+                },
+                "datatype": "json"
+            },
+            "language": {
+                "sProcessing": "<span class='fa fa-spin fa-gear fa-2x'></span>",
+                "sLengthMenu": "Tampilkan _MENU_ " + unit+"&nbsp;&nbsp;",
+                "sZeroRecords": "Opps, Tidak ditemukan " + unit + " satupun yang sesuai dengan pencarian/filter kamu",
+                "sInfo": "Menampilkan _START_-_END_ dari _TOTAL_ " + unit,
+                "sInfoEmpty": "Menampilkan 0-0 dari 0 " + unit,
+                "sSearch": "Cari "+ unit+": ",
+                "sInfoFiltered": "(disaring dari _MAX_ " + unit + " keseluruhan)",
+                "oPaginate": {
+                    "sFirst": "Awal",
+                    "sPrevious": '<i class="fa fa-angle-left">',
+                    "sNext": '<i class="fa fa-angle-right">',
+                    "sLast": "Akhir"
+                },
+                buttons: {
+                    colvis: 'Kolom'
+                }
+            },
+            //"columns": ,
+            "columns": read_column,
+            "lengthMenu": [[10, 50, 100, 1000], ['10', '50', '100', '1000']],
+            //initComplete: function (settings, json) {
+            //    console.log(settings);
+            //    //if (json.length) {
+            //    //    $(document).find('#inputsirupv2').removeClass('d-none');
+            //    //}
+                
+            ////    setTimeout(function () {
+            ////        table.page(parseInt(localStorage.lastpage) - 1).draw('page');
+            ////    }, 10);
+            //}
+        });
+
+        ////////DATATABLE SEARCH
+        function DT_search() {
+            $('#DT_search').on('paste keyup', function () {
+                if ($(this).val().length > 2) {
+                    table.search($(this).val()).draw();
+                }
+                if ($(this).val().length === 0) {
+                    table.search("").draw();
+                }
+            });
+        }
+
+        ///////DATATABLE PAGE LIMIT
+        function DT_limitpage() {
+            $('.DT_limitpage').append($('#DT_Table_length'));
+            $('.DT_limitpage').on('change', function () {
+                table.page.len($(this).val()).draw();
+            });
+        }
+
+        DT_draw();
+        DT_search();
+        DT_limitpage();
+        
+        function DT_draw() {
+            ////////DATATABLE DRAW CONDITION
+            $('#DT_Table').on('draw.dt', function (setting, json) {
+                if (json.aoData.length && getUrlVars()["p"] == "SIPKD_SIRUPV2") {
+                    $(document).find('#inputsirupv2').removeClass('d-none');
+                }
+                else {
+                    $(document).find('#inputsirupv2').addClass('d-none');
+                }
+                rememberpage();
+                Update();
+                Delete();
+                checkbox();
+
+                $(".checklistAll").prop('checked', false);
+                $("input:checkbox.checklist").prop('checked', false).trigger('change');
+                $("#aksicheck").addClass('hidden');
+            });
+        }
+        /////////////////////////////DATATABLES END////////////////////////////////////
+    }
+
+
+    ////////////////////////////////////////////////////////READ END////////////////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////////////////CREATE BEGIN////////////////////////////////////////////////////////
+    function Create() {
+        $("#btn_tambah").on('click', function () {
+            $(".JudulForm").text("Tambah " + unit);
+            var form = $('#form_modal2');
+            form.attr("action", "../CRUD/C_Table");
+            $('#btn_ubahdata').addClass('hidden');
+            $('#btn_simpandatabaru').removeClass('hidden');
+            //$('#edit_UNITKEY').val($('#DT_filter1').val()).trigger('change');
+        });
+    }
+    
+
+    //SIMPAN DATA BARU
+    $("#btn_simpandatabaru").on('click', function (e) {
+        //$('#edit_ID').val($('#edit_NIP').val());
+        var form = $('#form_modal2');
+        var validator = form.data("bs.validator");
+        validator.validate();
+        if (!validator.hasErrors()) {
+            var queryform = form.serialize() + '&tb=' + CRUDtbl + "&t=" + $.cookie('t');
+            $.ajax({
+                type: "POST",
+                url: form.attr('action') + "?" + queryform,
+                dataType: 'json',
+                success: function (response) {
+                    $('.close').click();
+                    if (response === "OK") {
+                        Command: toastr["success"]("Berhasil ditambahkan ke database", "Berhasil !")
+                        table.page(parseInt(localStorage.lastpage) - 1).draw('page');
+                    } else {
+                        Command: toastr["error"]("Data <strong> GAGAL </strong> ditambahkan ke database", "Error !")
+                    }
+                }
+            });
+        }
+    });
+    ////////////////////////////////////////////////////////CREATE END////////////////////////////////////////////////////////
+    
+    
+    ////////////////////////////////////////////////////////UPDATE BEGIN////////////////////////////////////////////////////////
+    function Update() {
+        $('.btn_edit').on('click', function () {
+            var ID = $(this).parent().siblings(".DT_ROWID").text().trim();
+            
+            $.ajax({
+                type: "GET",
+                url: '../CRUD/R_Proc',
+                data: 'SP=' + CRUDtbl + '_DET_BYID&ID=' + ID + "&t=" + $.cookie('t'),
+                success: function (response) {
+                    var data = $.parseJSON(response);
+                    //$("#edit_" + CRUDPk).val(ID).trigger('change');
+                    //console.log(data);
+
+                    $.each(data[0], function (index, value) {
+                        $('#edit_' + index).val(value).trigger('change');
+                        //var idchained = [];
+                        setTimeout(function () {
+                            $(".chained").each(function () {
+                                //idchained.push($(this).attr('name'));
+                                $("#edit_" + $(this).attr('name')).val(value).trigger('change')
+                            });
+                            
+                        }, 200);
+                    });
+                }
+            });
+
+            $(".JudulForm").text("Ubah Data " + unit);
+            $('#btn_simpandatabaru').addClass('hidden');
+            $('#btn_ubahdata').removeClass('hidden');
+        });
+    }
+
+    //UBAH DATA KE DATABASE
+    $("#btn_ubahdata").on('click', function () {
+        var ID = $("#edit_ID").val();
+        var form = $('#form_modal2');
+        var validator = form.data("bs.validator");
+        validator.validate();
+        if (!validator.hasErrors()) {
+            var queryform = form.serialize() + "&tb=" + CRUDtbl + "&pk=ID&ID=" + ID + "&t=" + $.cookie('t');
+            $.ajax({
+                type: "POST",
+                url: '../CRUD/U_Table?' + queryform,
+                dataType: 'json',
+                success: function (response) {
+                    if (response === "OK") {
+                        $('.close').click();
+                        Command: toastr["success"]("Berhasil Diubah", "Berhasil !")
+                        table.page(parseInt(localStorage.lastpage) - 1).draw('page');
+                    } else {
+                        Command: toastr["error"]("Data <strong> GAGAL </strong> diubah", "Error !")
+                    }
+                }
+            });
+        }
+    });
+    ////////////////////////////////////////////////////////UPDATE END////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////DELETE BEGIN////////////////////////////////////////////////////////
+    function Delete() {
+        //var ID;
+        ////SINGLE DELETE
+        $('.btn_hapus').on('click', function () {
+            var ID = $(this).parent().siblings(".DT_ROWID").text().trim();
+            swal({
+                title: 'Yakin mau dihapus?',
+                text: "Hati-hati kamu gak bisa ngembaliin data ini lagi!",
+                type: 'warning',
+                showCancelButton: true,
+                cancelButtonText: 'Batal',
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Ya, Hapus!'
+            }).then((result) => {
+                if (result.value) {
+                    $.ajax({
+                        type: "POST",
+                        url: '../CRUD/D_Table?tb=' + CRUDtbl + '&ID=' + ID + "&t=" + $.cookie('t'),
+                        success: function (response) {
+                            if (response === "OK") {
+                                Command: toastr["success"]("Data Barang telah dihapus", "OK !")
+                                table.page(1).draw('page');
+
+                            } else {
+                                Command: toastr["error"]("Data Barang <strong> GAGAL </strong> dihapus", "Error !")
+                            }
+                        }
+                    });
+                }
+            })
+        });
+
+        ////MULTI DELETE
+        $('#aksicheck').on('click', function () {
+            var id = [];
+
+            $('#DT_Table').find('tbody').find('.checked').each(function () {
+                id.push($(this).parent().siblings('.DT_ROWID').text().trim());
+            });
+            //console.log(id);
+
+            swal({
+                title: 'Yakin mau dihapus?',
+                text: "Hati-hati kamu gak bisa ngembaliin data ini lagi!",
+                type: 'warning',
+                showCancelButton: true,
+                cancelButtonText: 'Batal',
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Ya, Hapus!'
+            }).then((result) => {
+                if (result.value) {
+                    $.ajax({
+                        type: "POST",
+                        url: '../CRUD/D_TableMulti',
+                        data: 'pk=ID&id=' + id + '&tb=' + CRUDtbl + "&t=" + $.cookie('t'),
+                        success: function (response) {
+                            Command: toastr["success"](response, "OK !")
+                            table.page(1).draw('page');
+                            $(this).addClass('hidden');
+                        }
+                    });
+                }
+            })
+
+        });
+
+    }
+
+    ////////////////////////////////////////////////////////DELETE END////////////////////////////////////////////////////////
+});
